e1480a6fb6fe88183255f134ea3603b0491c24b9
TanPM77 <TanPhamm2707@gmail.com>
2020-07-21 08:02:06 +0700

    3rd: add JUnit class and .jar files

--- src/mathutil/se1408/MathUtilSE1408.java
+++ src/mathutil/se1408/MathUtilSE1408.java
@@ -16,7 +16,38 @@ public class MathUtilSE1408 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        
+        //expected : menthod would return 120 when input 5
         System.out.println("5! = " + computerFactorial(5));
+        //expected : menthod would return 720 when input 6
+        System.out.println("5! = " + computerFactorial(6));
+        //expected : menthod would return 1 when input 0
+        System.out.println("5! = " + computerFactorial(0));
+        //expected : menthod would return 1 when input 1
+        System.out.println("5! = " + computerFactorial(1));
+        //expected : menthod would return EXCEPTON when input -5
+        System.out.println("5! = " + computerFactorial(-5));
+        //Code contains method main(), we are writting code which call test app
+        //before doing this, we need prepare :
+        //1.have data : 5, 6, 0, 1, -5
+        //2.have method
+        // EXPECTED VALUE
+        
+        //3.call query, call app, call method run with data has just prepared
+        //view return result menthod,
+        //EX : cF(6) return 120 => false;
+        
+        //Test 1 : simple but not enough
+        //cause : view by eyes, tu so sanh giua gia tri ve ham
+        //va ki vong
+        //them nua no cam minh disable nut clean & build
+        //code sai ve logic nhung van ra file.jar, .war
+        //ta can 1 dung cach khac dam bao chat luong code khi viet code
+        //ta nap them thu vien phu tro kiem soat giup kiem soat chat luong khi viet code
+        //do la ki thuat TDD kem UNIT TESTING
+        //xai thu vien JUNIT, NUnit, TestNG,
+        //green is ok, red is not ok
     }
 
 }
---
+++ test/tdd/MathUnitTest.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tdd;
+
+import org.junit.Assert;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import tan.util.MathUtil;
+
+/**
+ *
+ * @author Minh Tan
+ */
+public class MathUnitTest {
+    //this medthods turn into main
+    @Test
+    public void testSuccessfulCase(){
+        //test case succedss means give the true data
+        //method cF(1)
+        //true    Assert.assertEquals(6, tan.util.MathUtil.computerFactorial(3));
+        assertEquals(1, MathUtil.computerFactorial(0));
+        assertEquals(1, MathUtil.computerFactorial(1));
+        assertEquals(2, MathUtil.computerFactorial(2));
+        assertEquals(6, MathUtil.computerFactorial(3));
+        assertEquals(24, MathUtil.computerFactorial(4));
+        assertEquals(120, MathUtil.computerFactorial(5));
+        
+    }
+    
+    @Test(expected = IllegalArgumentException.class) // @Test turn a method into main to run if click Shift + F6
+    public void testFailedCase(){
+        //test case fail means give the wrong data
+        //cF(-5): nagative number can not ouput result , cF(20) : result number is to big can not print,
+        //exception is cases with no data before, it is not value
+        //so can not compare , can not use assertEquals() because it compare between values
+        //We need use other option with exception
+        //i need result Exception when we input value = -5 into cF()
+        //so print out String which is green or red
+        MathUtil.computerFactorial(-5);
+    }
+}
+
+//when you code is wrong logic but netbean can build file .jar, .war when Clean & build
+//it is not safe
+//we disable clean & build if test code is red
+//when use JUnit, we can use this
+//in Java Desktop sout(), JOptionPane, write file log in Java Web
+//but any thing can build file .jar, .war
+//NB 8.2 1005.
